aa87c56c9f9da30b8e57959929edba6ba182d2e7
dipangshu22 <dipangshu22@gmail.com>
2025-07-28 15:55:39 +0530

    changes

--- script.js
+++ script.js
@@ -20,6 +20,41 @@ let quiz=[
 ]
 
 let question_text=document.getElementById("question_text")
-let answers=document.getElementsByClassName("btn")
-let next=document.getElementsByClassName("next")
+let answers_btn=document.getElementById("answers")
+let next=document.getElementById("next")
+
+
+function toggleContent() {
+      var content = document.getElementById("quiz")
+      if (content.style.display === "none") {
+        content.style.display = "block"; // show
+      } else {
+        content.style.display = "none"; // hide
+      }
+    }
+
+function startquiz(){
+    questionindex=0;
+    score=0
+    next.innerHTML="next";
+    showquestion();
+   
+}
+
+ function showquestion(){
+    let currentquestion=quiz[questionindex];
+    let question_no=currentquestion+1;
+    question_text.innerText=question_no+" . "+currentquestion.question;
+
+    currentquestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answers_btn.appendChild(button);
+
+    });
+}
+
+startquiz()
+
 
